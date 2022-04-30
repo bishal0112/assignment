a22a3f75f2db29be3c967d47449ed2e225e9ea98
bishal0112 <bishalknight5440@gmail.com>
2022-04-30 10:36:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var canvas = new fabric.Canvas("canvas");
+document.getElementById("uploader").onchange = function (e) {
+	var reader = new FileReader();
+	reader.onload = function (e) {
+		var image = new Image();
+		image.src = e.target.result;
+		image.onload = function () {
+			var img = new fabric.Image(image);
+			img.set({
+				left: 50,
+				top: 0,
+			});
+			img.scaleToWidth(400);
+			canvas.add(img).setActiveObject(img).renderAll();
+		};
+	};
+	reader.readAsDataURL(e.target.files[0]);
+};
+
+canvas.on("mouse:wheel", function (opt) {
+	var delta = opt.e.deltaY;
+	var zoom = canvas.getZoom();
+	zoom *= 0.999 ** delta;
+	if (zoom > 20) zoom = 20;
+	if (zoom < 0.01) zoom = 0.01;
+	canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+	opt.e.preventDefault();
+	opt.e.stopPropagation();
+});
