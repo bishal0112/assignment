381e7b52ff8c76bc22af08e3ca673b6de423df09
bishal0112 <bishalknight5440@gmail.com>
2022-05-01 20:19:06 +0530

    Second commit

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ canvas.on("mouse:wheel", function (opt) {
 	var zoom = canvas.getZoom();
 	zoom *= 0.999 ** delta;
 	if (zoom > 20) zoom = 20;
-	if (zoom < 0.01) zoom = 0.01;
-	canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+	if (zoom < 1) zoom = 1;
+	canvas.setZoom(zoom);
+	// canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
 	opt.e.preventDefault();
 	opt.e.stopPropagation();
 });
